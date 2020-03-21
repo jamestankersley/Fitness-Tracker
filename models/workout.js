fa1458992293589f4cc6fd7bac92b31ae7942ad0
James <jwtankersley10@gmail.com>
2020-03-21 11:29:56 -0500

    changed models

--- models/workout.js
+++ models/workout.js
@@ -1,15 +1,57 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ExerciseSchema = require("./exercise.js");
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
   },
-  exercises: [ExerciseSchema]
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
