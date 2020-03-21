08b8d3cd37a88a75fd13cfa47336a3f23c4fe2b0
James <jwtankersley10@gmail.com>
2020-03-21 12:14:50 -0500

    updates

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
+
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8080;
-
-const db = require("./models");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Headlines";
+mongoose.connect(MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
-  useNewUrlParser: true
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-require("./routes/html-routes")(app);
-require("./routes/api-routes")(app);
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
 });
